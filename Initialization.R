2244e4b19912591fd8a670080701e132ea70994f
bitasha <shams.bita@yahoo.com>
2018-11-03 17:31:32 +0330

    Add files via upload

---
+++ Initialization.R
@@ -0,0 +1,16 @@
+rm(list=ls())
+setwd("~/IteRank")
+
+source(paste(getwd(),'/Common code.R',sep=""))
+source(paste(getwd(),'/IteRank.R',sep=""))
+
+
+
+rating<- read.delim(paste(getwd(),'/rating.data',sep=""), header=FALSE)
+trdir=paste(getwd(),'/tr-1-10.csv',sep="")
+simdir=paste(getwd(),'/sim-tr-1-10.csv',sep="")
+
+rmat=makeRatmat(rating)
+usersequence=makeuserSequence(rating);
+res2=rankEvaluation_Yu_IteRank(rmat,userSeq = usersequence,trl=10,direc = "~/g-res.csv",firstRun = 1,damping = 0.85,trainflag = 0,trdirec = trdir)
+
